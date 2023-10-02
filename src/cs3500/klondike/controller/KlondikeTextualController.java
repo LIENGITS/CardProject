b5915b74572fa0502c20b71179b0caf865e50bc6
GITLIEN <60283203+LIENGITS@users.noreply.github.com>
2023-10-02 06:31:53 -0400

    Add files via upload

---
+++ src/cs3500/klondike/controller/KlondikeTextualController.java
@@ -0,0 +1,16 @@
+package cs3500.klondike.controller;
+
+import cs3500.klondike.model.hw02.KlondikeModel;
+
+import java.util.List;
+
+public class KlondikeTextualController implements cs3500.klondike.controller.KlondikeController {
+  public KlondikeTextualController(Readable r, Appendable a) {
+    throw new RuntimeException("If you got this to run, your tests compile correctly against the data definitions.");
+  }
+
+  @Override
+  public void playGame(KlondikeModel model, List<Card> deck, boolean shuffle, int numPiles, int numDraw) {
+    throw new RuntimeException("If you got this to run, your tests compile correctly against the data definitions.");
+  }
+}
